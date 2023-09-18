f1779106d3aa9af63a43a56704d9d8f86d6a8678
Romamill <122994080+Romamill@users.noreply.github.com>
2023-09-18 21:57:32 +0300

    create

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,8 @@
+import { HeaderDiv, HeaderH1, HeaderH2 } from './AppNew.styled';
+import Filter from './ContactFilters/ContactFilters';
+import ContactFormPage from './ContactForm/ContactForm';
+import ContactListPage from './ContactList/ContactsList';
+
 export const App = () => {
   return (
     <HeaderDiv>
